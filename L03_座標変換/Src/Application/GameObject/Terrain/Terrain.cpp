d1ba0488b8b35214b60b71cc23ede9d7047f7d77
riku-na <kd1370921@st.kobedenshi.ac.jp>
2024-05-27 14:29:53 +0900

    座標変換
    ワールド座標(3D)からスクリーン座標(2D)への変換

---
+++ L03_座標変換/Src/Application/GameObject/Terrain/Terrain.cpp
@@ -0,0 +1,22 @@
+﻿#include "Terrain.h"
+
+void Terrain::Init()
+{
+	if (!m_spModel)
+	{
+		m_spModel = std::make_shared<KdModelWork>();
+		m_spModel->SetModelData("Asset/Data/LessonData/Terrain/Terrain.gltf");
+	}
+}
+
+void Terrain::Update()
+{
+}
+
+void Terrain::DrawLit()
+{
+	if (!m_spModel) return;
+
+	KdShaderManager::Instance().m_StandardShader.DrawModel(*m_spModel);
+}
+
